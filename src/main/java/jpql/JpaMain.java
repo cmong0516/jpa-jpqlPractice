299cdf41fbfafcc2bb4295bd5b6d42db58a51c00
cmong0516 <cmong0516@gmail.com>
2022-08-21 14:36:19 +0900

    2022.08.21

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,33 @@
+package jpql;
+
+import javax.persistence.*;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+//            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username=:username", Member.class);
+//            query.setParameter("username", "member1");
+//            Member result = query.getSingleResult();
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class).setParameter("username", "member1").getSingleResult();
+            System.out.println("result = " + result.getUsername());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        }
+
+        em.close();
+        emf.close();
+    }
+}
