4c9bd10343afc72dfc0f498aa814d2ae44b2063a
cmong0516 <cmong0516@gmail.com>
2022-08-22 13:48:54 +0900

    2022.08.22

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -44,45 +44,87 @@ public class JpaMain {
 //            em.createQuery("select m from Member m where exists (select t from m.team t where t.name = '팀A')");
 ////            팀에 소속된 회원
 //            em.createQuery("select m from Member m where m.team = ANY ");
-            Team team = new Team();
-            team.setName("teamA");
+//            Team team = new Team();
+//            team.setName("teamA");
+//
+//            em.persist(team);
+//
+//            Member member = new Member();
+//            member.setUsername("관리자1");
+//            member.setTeam(team);
+//            em.persist(member);
+//
+//            Member member1 = new Member();
+//            member1.setUsername("관리자2");
+//            member1.setTeam(team);
+//            em.persist(member1);
+//
+//
+//
+////            String sql = "select m.username from Member m";
+////            List<String> result = em.createQuery(sql, String.class).getResultList();
+////            // m.username : 상태필드 경로탐색의 끝.
+//
+////            String sql = "select m.team from Member m";
+////
+////            List<String> result = em.createQuery(sql, String.class).getResultList();
+//            // m.team : 단일 값 연관 경로
+//            // 묵시적 내부조인 발생 탐색 o
+////            String sql1 = "select m.team.name from Member m";
+//
+//            String sql = "select t.members From Team t";
+//
+//            // 컬렉션 값 연관 경로
+//            // 묵시적 내부 조인 발생 , 탐색 x
+//            //기존 sql 수정
+//            String sql1 = "select m.username from Team t join t.members m";
+//            List<String> result = em.createQuery(sql1, String.class).getResultList();
+//
+//            System.out.println("result = " + result);
+//
+//            // 묵시적 조인 말고 명시적 조인을 사용하자.
 
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("teamA");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("관리자1");
-            member.setTeam(team);
-            em.persist(member);
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자2");
-            member1.setTeam(team);
+            member1.setUsername("member1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(teamA);
+            em.persist(member2);
 
+            Member member3 = new Member();
+            member3.setUsername("member3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
-//            String sql = "select m.username from Member m";
-//            List<String> result = em.createQuery(sql, String.class).getResultList();
-//            // m.username : 상태필드 경로탐색의 끝.
+            em.flush();
+            em.clear();
 
-//            String sql = "select m.team from Member m";
-//
-//            List<String> result = em.createQuery(sql, String.class).getResultList();
-            // m.team : 단일 값 연관 경로
-            // 묵시적 내부조인 발생 탐색 o
-//            String sql1 = "select m.team.name from Member m";
+//            String sql = "select m from Member m";
+            String sql = "select m from Member m join fetch m.team";
+            // n+1 문제를 해결하기 위해 sql 변경.
+            // 3번 날아가던 쿼리가 1번만 날아감.
 
-            String sql = "select t.members From Team t";
+            List<Member> result = em.createQuery(sql, Member.class).getResultList();
 
-            // 컬렉션 값 연관 경로
-            // 묵시적 내부 조인 발생 , 탐색 x
-            //기존 sql 수정
-            String sql1 = "select m.username from Team t join t.members m";
-            List<String> result = em.createQuery(sql1, String.class).getResultList();
+            for (Member member : result) {
+                System.out.println("member = " + member.getUsername() + ", " +member.getTeam().getName());
+            }
 
-            System.out.println("result = " + result);
-
-            // 묵시적 조인 말고 명시적 조인을 사용하자.
+            // 회원1 팀A SQL
+            // 회원2 팀A 1차캐시
+            // 회원3 팀B SQL
+            // 회원 100명 -> N+1 발생.
 
             tx.commit();
         } catch (Exception e) {
