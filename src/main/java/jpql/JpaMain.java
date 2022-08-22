b9da303dbbcf50506d17c497d4a766f7978be885
cmong0516 <cmong0516@gmail.com>
2022-08-22 12:35:20 +0900

    2022.08.22

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -43,6 +44,45 @@ public class JpaMain {
 //            em.createQuery("select m from Member m where exists (select t from m.team t where t.name = '팀A')");
 ////            팀에 소속된 회원
 //            em.createQuery("select m from Member m where m.team = ANY ");
+            Team team = new Team();
+            team.setName("teamA");
+
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("관리자1");
+            member.setTeam(team);
+            em.persist(member);
+
+            Member member1 = new Member();
+            member1.setUsername("관리자2");
+            member1.setTeam(team);
+            em.persist(member1);
+
+
+
+//            String sql = "select m.username from Member m";
+//            List<String> result = em.createQuery(sql, String.class).getResultList();
+//            // m.username : 상태필드 경로탐색의 끝.
+
+//            String sql = "select m.team from Member m";
+//
+//            List<String> result = em.createQuery(sql, String.class).getResultList();
+            // m.team : 단일 값 연관 경로
+            // 묵시적 내부조인 발생 탐색 o
+//            String sql1 = "select m.team.name from Member m";
+
+            String sql = "select t.members From Team t";
+
+            // 컬렉션 값 연관 경로
+            // 묵시적 내부 조인 발생 , 탐색 x
+            //기존 sql 수정
+            String sql1 = "select m.username from Team t join t.members m";
+            List<String> result = em.createQuery(sql1, String.class).getResultList();
+
+            System.out.println("result = " + result);
+
+            // 묵시적 조인 말고 명시적 조인을 사용하자.
 
             tx.commit();
         } catch (Exception e) {
